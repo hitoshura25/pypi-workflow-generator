c91f61757f25aed3f028cfe12a1314e10900a842
Vinayak Menon <vviswana@gmail.com>
2025-10-30 16:34:25 -0500

    Trying this out

--- pypi_workflow_generator/tests/test_generator.py
+++ pypi_workflow_generator/tests/test_generator.py
@@ -27,9 +27,6 @@ def test_generate_workflow_default_arguments(tmp_path):
 
     if_line = next(line for line in publish_pypi_block.splitlines() if line.strip().startswith("if:"))
     assert "".join(expected_if_condition.split()) == "".join(if_line.split())
-    assert "- name: Set pre--release version for TestPyPI" in content
-    assert 'LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null) || LATEST_TAG="0.0.0"' in content
-    assert 'echo "SETUPTOOLS_SCM_PRETEND_VERSION=${LATEST_TAG}.dev{{ github.run_id }}" >> $GITHUB_ENV' in content
 
 def test_generate_workflow_custom_arguments(tmp_path):
     """Test workflow generation with custom arguments."""
@@ -57,6 +54,3 @@ def test_generate_workflow_custom_arguments(tmp_path):
       - 'v*.*.*'""" not in content
     assert "startsWith(github.ref, 'refs/tags')" not in "".join(content.split())
     assert "verbose: true" in content
-    assert "- name: Set pre--release version for TestPyPI" in content
-    assert 'LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null) || LATEST_TAG="0.0.0"' in content
-    assert 'echo "SETUPTOOLS_SCM_PRETEND_VERSION=${LATEST_TAG}.dev{{ github.run_id }}" >> $GITHUB_ENV' in content
--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pypi-workflow-generator',
-    use_scm_version={'local_scheme': 'no-local-version', 'fallback_version': '0.0.1'},
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),

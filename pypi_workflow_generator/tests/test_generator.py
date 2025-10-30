9bb7f544b249aa990f09625470d3654868f78ee0
Vinayak Menon <vviswana@gmail.com>
2025-10-30 16:14:54 -0500

    Trying to get PR builds to have unique versions

--- pypi_workflow_generator/tests/test_generator.py
+++ pypi_workflow_generator/tests/test_generator.py
@@ -29,6 +29,9 @@ def test_generate_workflow_default_arguments(tmp_path):
     assert "".join(expected_if_condition.split()) == "".join(if_line.split())
     assert "github.ref == 'refs/heads/main'" not in "".join(content.split())
     assert "verbose: true" not in content
+    assert "- name: Set pre-release version for TestPyPI" in content
+    assert "LATEST_TAG=$(git describe --tags --abbrev=0)" in content
+    assert 'echo "SETUPTOOLS_SCM_PRETEND_VERSION=${LATEST_TAG}.dev{{ github.run_id }}" >> $GITHUB_ENV' in content
 
 def test_generate_workflow_custom_arguments(tmp_path):
     """Test workflow generation with custom arguments."""
@@ -56,3 +59,6 @@ def test_generate_workflow_custom_arguments(tmp_path):
       - 'v*.*.*'""" not in content
     assert "startsWith(github.ref, 'refs/tags')" not in "".join(content.split())
     assert "verbose: true" in content
+    assert "- name: Set pre-release version for TestPyPI" in content
+    assert "LATEST_TAG=$(git describe --tags --abbrev=0)" in content
+    assert 'echo "SETUPTOOLS_SCM_PRETEND_VERSION=${LATEST_TAG}.dev{{ github.run_id }}" >> $GITHUB_ENV' in content

77a17f098255aed72dd8eea69c097efa45cb9856
Vinayak Menon <vviswana@gmail.com>
2025-10-30 13:50:21 -0500

    Using "use_scm_version"

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='pypi-workflow-generator',
-    version='0.1.0',
+    use_scm_version=True,
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),

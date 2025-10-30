ff8cf535649364a94116003867eb362d482e74b6
Vinayak Menon <vviswana@gmail.com>
2025-10-30 16:50:19 -0500

    Now trying this

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 
 from setuptools import setup, find_packages
+import os
+
+def local_scheme(version):
+    if os.environ.get("IS_PULL_REQUEST"):
+        return f".dev{os.environ['GITHUB_RUN_ID']}"
+    else:
+        return ""
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name='pypi-workflow-generator',
+    use_scm_version={"local_scheme": local_scheme},
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),

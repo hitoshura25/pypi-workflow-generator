174fb84d7c1e8148268200386c0359852589e2ab
Vinayak Menon <vviswana@gmail.com>
2025-10-30 16:21:05 -0500

    Getting PR builds working with unique versions

--- pypi_workflow_generator/tests/test_generator.py
+++ pypi_workflow_generator/tests/test_generator.py
@@ -27,10 +27,8 @@ def test_generate_workflow_default_arguments(tmp_path):
 
     if_line = next(line for line in publish_pypi_block.splitlines() if line.strip().startswith("if:"))
     assert "".join(expected_if_condition.split()) == "".join(if_line.split())
-    assert "github.ref == 'refs/heads/main'" not in "".join(content.split())
-    assert "verbose: true" not in content
-    assert "- name: Set pre-release version for TestPyPI" in content
-    assert "LATEST_TAG=$(git describe --tags --abbrev=0)" in content
+    assert "- name: Set pre--release version for TestPyPI" in content
+    assert 'LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null) || LATEST_TAG="0.0.0"' in content
     assert 'echo "SETUPTOOLS_SCM_PRETEND_VERSION=${LATEST_TAG}.dev{{ github.run_id }}" >> $GITHUB_ENV' in content
 
 def test_generate_workflow_custom_arguments(tmp_path):
@@ -59,6 +57,6 @@ def test_generate_workflow_custom_arguments(tmp_path):
       - 'v*.*.*'""" not in content
     assert "startsWith(github.ref, 'refs/tags')" not in "".join(content.split())
     assert "verbose: true" in content
-    assert "- name: Set pre-release version for TestPyPI" in content
-    assert "LATEST_TAG=$(git describe --tags --abbrev=0)" in content
+    assert "- name: Set pre--release version for TestPyPI" in content
+    assert 'LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null) || LATEST_TAG="0.0.0"' in content
     assert 'echo "SETUPTOOLS_SCM_PRETEND_VERSION=${LATEST_TAG}.dev{{ github.run_id }}" >> $GITHUB_ENV' in content
